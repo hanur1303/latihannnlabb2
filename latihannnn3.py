6ec9c2c1ae5c900fc8eb93ad99d6bb1321b46954
hanur1303 <hanur1303@gmail.com>
2023-10-24 18:29:23 +0700

    latihannnnlabb2py

---
+++ latihannnn3.py
@@ -0,0 +1,16 @@
+#input nilai variabel
+a = input("masukkan nilai a:")
+b = input("masukkan nilai b:")
+
+#cetak nilai variabel
+print("variabel a=",a)
+print("variabel b=",b)
+
+#cetak hasil
+print("hasil penggabungan {} & {} = {}".format(a, b, a + b ))
+
+#konversi nilai variabel 
+a = int(a)
+b = int(b)
+print("hasil penjumlahan {} + {} = {}".format(a, b, a + b))
+print("hasil pembagian {} / {} = {}".format(a, b, a / b))
